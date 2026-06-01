6651f5053823fc2f743188ab6e2c6706128cbed2
Alumno Torre Tecnológica <alu_torre1@instructores.senati.local>
2026-06-01 16:44:36 -0500

    a

--- backup.php
+++ backup.php
@@ -1,20 +1,20 @@
 <?php
 
-date_default_timezone_set('America/Lima');
+date_default_timezone_set('America/Lima'); # Linux configurado 
 
-$origen = __DIR__ . "/documentos";
+$origen = __DIR__ . "/documentos"; # REntramos la ruta documentos txt1 - txt2
 
-$fecha = date("Ymd_His");
+$fecha = date("Ymd_His"); # Formato Año - Mes y Dia
 
-$destino = __DIR__ . "/backups/backup_" . $fecha . ".zip";
+$destino = __DIR__ . "/backups/backup_" . $fecha . ".zip"; 
 
-$zip = new ZipArchive();
+$zip = new ZipArchive();  
 
-if($zip->open($destino, ZipArchive::CREATE) === TRUE){
+if($zip->open($destino, ZipArchive::CREATE) === TRUE){ # Se pregunta el Zip
 
-    $archivos = scandir($origen);
+    $archivos = scandir($origen); # Origen del zip
 
-    foreach($archivos as $archivo){
+    foreach($archivos as $archivo){ #Recorre todos los archivos txt - comprimirlo en zip
 
         if($archivo != "." && $archivo != ".."){
 
